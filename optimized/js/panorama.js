833c4fa737dd8fbb9cc99c719676ddf56b2af11c
Jay Price <jxprice@live.com>
2019-06-25 15:31:10 -0400

    Just make the panoramas work on mobile by bumping all the sizes down to 4096xPROPORTION. Desktop will render with less definition, but site should function on all platforms.

--- optimized/js/panorama.js
+++ optimized/js/panorama.js
@@ -1,23 +1,23 @@
 pannellum.viewer('multnomahFalls', {
     "type": "equirectangular",
-    "panorama": "https://s3.amazonaws.com/proprtechnologysolutions/content/landmarks/desktop/multnomahFallsMultnomahCountyOR97014.jpg"
+    "panorama": "https://s3.amazonaws.com/proprtechnologysolutions/content/landmarks/mobile/multnomahFallsMultnomahCountyOR97014.jpg"
 });
 pannellum.viewer('percivalLanding', {
     "type": "equirectangular",
-    "panorama": "https://s3.amazonaws.com/proprtechnologysolutions/content/landmarks/desktop/301ColumbiaStreetNWOlympiaWA98501.jpg"
+    "panorama": "https://s3.amazonaws.com/proprtechnologysolutions/content/landmarks/mobile/301ColumbiaStreetNWOlympiaWA98501.jpg"
 });
 
 pannellum.viewer('broadStreet', {
     "type": "equirectangular",
-    "panorama": "https://s3.amazonaws.com/proprtechnologysolutions/content/landmarks/desktop/bloomfield/BroadStreet%26LibertyStreetBloomfieldNJ07003-25m.jpg"
+    "panorama": "https://s3.amazonaws.com/proprtechnologysolutions/content/landmarks/mobile/bloomfield/BroadStreet%26LibertyStreetBloomfieldNJ07003-25m.jpg"
 });
 
 pannellum.viewer('driveway', {
     "type": "equirectangular",
-    "panorama": "https://s3.amazonaws.com/proprtechnologysolutions/content/landmarks/desktop/116PompanoRoadWoodlandWA98674.jpg"
+    "panorama": "https://s3.amazonaws.com/proprtechnologysolutions/content/landmarks/mobile/116PompanoRoadWoodlandWA98674.jpg"
 });
 
 pannellum.viewer('crossroads', {
     "type": "equirectangular",
-    "panorama": "https://s3.amazonaws.com/proprtechnologysolutions/content/landmarks/desktop/runyanRoad%26PompanoRoadWoodlandWA98674.jpg"
+    "panorama": "https://s3.amazonaws.com/proprtechnologysolutions/content/landmarks/mobile/runyanRoad%26PompanoRoadWoodlandWA98674.jpg"
 });
